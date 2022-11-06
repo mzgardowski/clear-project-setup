8f979620af4fae96d9a2d6e5775bdca51d692749
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-11-06 12:20:25 +0100

    Basic Project Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { FC } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App: FC = () => {
   return (
@@ -20,7 +20,7 @@ const App: FC = () => {
         </a>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
